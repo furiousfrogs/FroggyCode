46bc2007191b746c7ac4fa8acc0f4c18ede41bae
furiousfrogs <furiousfrogsrobotics@gmail.com>
2025-10-12 13:09:10 -0700

    colour sensor added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/vision/rotatetest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/vision/rotatetest.java
@@ -14,31 +14,36 @@ import org.firstinspires.ftc.teamcode.hardware.Globals;
 
 @TeleOp(name = "rotate test")
 public class rotatetest extends OpMode {
- private DcMotor revolver;
+ private Motor revolver;
     private GamepadEx gamepadEx;
     @Override
     public void init() {
 
 
-        revolver = hardwareMap.get(DcMotor.class, "revolver");
-        revolver.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        revolver.setTargetPosition(0);
-        revolver.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //revolver = hardwareMap.get(DcMotor.class, "revolver");
+        //revolver.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //revolver.setTargetPosition(0);
+        //revolver.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        //revolver = new Motor(hardwareMap, "revolver", 1538.0, 435);
-        //revolver.setRunMode(Motor.RunMode.PositionControl);
-        //revolver.setPositionCoefficient(Globals.revolverKP);
+        revolver = new Motor(hardwareMap, "revolver", 1538.0, 435);
+        revolver.setRunMode(Motor.RunMode.PositionControl);
+        revolver.setPositionCoefficient(Globals.revolverKP);
         gamepadEx = new GamepadEx(gamepad1);
     }
 
     @Override
     public void loop(){
-
+        revolver.setPositionCoefficient(Globals.revolverKP);
+        revolver.setPositionTolerance(Globals.revolverTol);
         if (gamepadEx.getButton(GamepadKeys.Button.SQUARE)) {
             revolver.setTargetPosition(revolver.getCurrentPosition() + Globals.oneRotation); // 2 rotations
         } else if (gamepadEx.getButton(GamepadKeys.Button.CROSS)) {
             revolver.setTargetPosition(revolver.getCurrentPosition() - Globals.oneRotation);
         }
-        revolver.setPower(0.8);
+        if (!revolver.atTargetPosition()) {
+            revolver.set(0.5);
+        } else {
+            revolver.set(0);
+        }
     }
 }
