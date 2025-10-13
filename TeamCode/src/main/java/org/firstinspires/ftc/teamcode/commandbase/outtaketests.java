b386ade72c1473d9c8b2dde8d3a79b3ba3952e42
furiousfrogs <furiousfrogsrobotics@gmail.com>
2025-10-12 21:06:13 -0700

    colour sensor added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/outtaketests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commandbase/outtaketests.java
@@ -1,20 +1,16 @@
-package org.firstinspires.ftc.teamcode.commandbase.vision;
-
-import android.provider.Settings;
+package org.firstinspires.ftc.teamcode.commandbase;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.seattlesolvers.solverslib.gamepad.GamepadEx;
 import com.seattlesolvers.solverslib.gamepad.GamepadKeys;
 import com.seattlesolvers.solverslib.hardware.motors.Motor;
-import com.seattlesolvers.solverslib.hardware.motors.MotorEx;
 
 import org.firstinspires.ftc.teamcode.hardware.Globals;
 
-@TeleOp(name = "rotate test")
-public class rotatetest extends OpMode {
- private Motor revolver;
+@TeleOp(name = "outtake")
+public class outtaketests extends OpMode {
+    private Motor revolver;
     private GamepadEx gamepadEx;
     @Override
     public void init() {
@@ -26,22 +22,18 @@ public class rotatetest extends OpMode {
         //revolver.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         revolver = new Motor(hardwareMap, "revolver", 1538.0, 435);
-        revolver.setRunMode(Motor.RunMode.PositionControl);
-        revolver.setPositionCoefficient(Globals.revolverKP);
+        revolver.setRunMode(Motor.RunMode.RawPower);
+
         gamepadEx = new GamepadEx(gamepad1);
     }
 
     @Override
     public void loop(){
-        revolver.setPositionCoefficient(Globals.revolverKP);
-        revolver.setPositionTolerance(Globals.revolverTol);
+
         if (gamepadEx.getButton(GamepadKeys.Button.SQUARE)) {
-            revolver.setTargetPosition(revolver.getCurrentPosition() + Globals.oneRotation); // 2 rotations
+          revolver.set(1);
         } else if (gamepadEx.getButton(GamepadKeys.Button.CROSS)) {
-            revolver.setTargetPosition(revolver.getCurrentPosition() - Globals.oneRotation);
-        }
-        if (!revolver.atTargetPosition()) {
-            revolver.set(0.5);
+           revolver.set(-1);
         } else {
             revolver.set(0);
         }
